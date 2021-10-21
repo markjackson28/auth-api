0d76440e29446404005c26817cd68e68725fdb3d
Mark <mark28ten@gmail.com>
2021-10-20 21:18:43 -0700

    merged both files and working

--- auth-server/src/auth/models/index.js
+++ auth-server/src/auth/models/index.js
@@ -1,13 +1,23 @@
 'use strict';
 
+// From AUTH-API
+const clothesModel = require('../../api/models/clothes/model');
+const foodModel = require('../../api/models/food/model');
+const Collection = require('../../api/models/data-collection');
+
 const userModel = require('./users.js');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory;';
 
 const sequelize = new Sequelize(DATABASE_URL);
+// From AUTH-API
+const food = foodModel(sequelize, DataTypes);
+const clothes = clothesModel(sequelize, DataTypes);
 
 module.exports = {
   db: sequelize,
   users: userModel(sequelize, DataTypes),
+  food: new Collection(food),
+  clothes: new Collection(clothes),
 }
--- auth-server/src/server.js
+++ auth-server/src/server.js
@@ -9,6 +9,10 @@ const morgan = require('morgan');
 const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
 const authRoutes = require('./auth/routes.js');
+// Added
+const logger = require('../src/auth/middleware/logger');
+const v1Routes = require('../src/api/routes/v1');
+const v2Routes = require('../src/api/routes/v2')
 
 // Prepare the express app
 const app = express();
@@ -16,15 +20,19 @@ const app = express();
 // App Level MW
 app.use(cors());
 app.use(morgan('dev'));
+app.use(logger);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use(authRoutes);
+// Added
+app.use('/api/v1', v1Routes);
+app.use('/api/v2', v2Routes);
 
 // Catchalls
-app.use(notFound);
+app.use('*', notFound);
 app.use(errorHandler);
 
 module.exports = {
